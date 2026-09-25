69f3f1bf308b70184cf57969534e757e5f4fe1ef
ClassicNeutrinoDust <evenoddquest@gmail.com>
2026-09-25 22:52:40 +0530

    Refactor settings.gradle.kts for plugin management
    Removed specific Maven repositories for plugin management and updated root project name.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,8 @@
 pluginManagement {
     repositories {
-        maven { url = uri("https://maven.fabricmc.net/") }
-        maven { url = uri("https://maven.neoforged.net/releases") }
         gradlePluginPortal()
         mavenCentral()
     }
 }
 
-rootProject.name = "useful_ores_26_3_workspace"
-
-includeBuild("fabric/26.3") {
-    name = "fabric-26.3"
-}
-
-includeBuild("neoforge/26.3") {
-    name = "neoforge-26.3"
-}
+rootProject.name = "useful-ores"
